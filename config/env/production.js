08468690b5d2d029fad545778418340755799c75
Bipin <bipin@logisticinfotech.co.in>
2019-07-13 17:56:51 +0530

    add datasource in production js file

--- config/env/production.js
+++ config/env/production.js
@@ -47,6 +47,9 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     default: {
+      adapter: require('sails-mysql'),
+      url: 'mysql://sailstestproject@mysql-sais-testproject:testProject@123@mysql-sais-testproject.mysql.database.azure.com:3306/sailstestdb',
+    
       // adapter: 'sails-mysql',
       // url: 'mysql://user:password@host:port/database',
       //--------------------------------------------------------------------------
